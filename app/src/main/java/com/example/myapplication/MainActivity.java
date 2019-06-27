02a705a01dcd5325a017774ee2696e0863ab0b7b
vlsantillan <vincentlawrencesantillan@gmail.com>
2019-06-27 10:07:24 +0800

    Transparent border

---
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.myapplication;
+
+
+import android.app.Activity;
+import android.os.Bundle;
+
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends Activity implements OnMapReadyCallback {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+//        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+//                .findFragmentById(R.id.map);
+//        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+
+    }
+}
